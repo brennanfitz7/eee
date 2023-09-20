21347d647cb810456767fb04b6c89c26de92bb97
Brennan <brennanfitz7@gmail.com>
2023-09-19 19:10:00 -0700

    bug fix for diff python version

--- eee/ddg/foldx/convert_to_df.py
+++ eee/ddg/foldx/convert_to_df.py
@@ -11,16 +11,20 @@ def convert_to_df(pdb_file:str):
     column_names=['Mutation','DDG']
     ddg_df=pd.read_csv(ddg_txt, sep='\t')
 
-    ddg_df.set_axis(column_names, axis=1, inplace=True)
-    for index,row in ddg_df.iterrows():
+    #got error message "DataFrame.set_axis() got an unexpected keyword argument 'inplace'" when run on spock
+    #seems like spock python is outdated--need to update it but not tonight
+    #ddg_df.set_axis(column_names, axis=1, inplace=True)
+    #quick fix for now--will update later and see what happens
+    ddg_df1=ddg_df.set_axis(column_names, axis=1)
+    for index,row in ddg_df1.iterrows():
         one_letter_code=AA_3TO1.get(row[0][0:3])
         if one_letter_code!=row[0][5] and row[0][5].isupper():
             #replacing the old mutation format with a new one
-            ddg_df.replace(row[0],one_letter_code+row[0][4]+row[0][5],inplace=True)
+            ddg_df1.replace(row[0],one_letter_code+row[0][4]+row[0][5],inplace=True)
         else:
-            ddg_df.drop(index,inplace=True)
+            ddg_df1.drop(index,inplace=True)
 
     output_df=pdb_id+'_ddg_df.csv'
-    ddg_df.to_csv(output_df, index=False)
+    ddg_df1.to_csv(output_df, index=False)
     
-    return ddg_df
+    return ddg_df1

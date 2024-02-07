a9779685cd0d8e3c96da910f3a5361603c186d70
Brennan <brennanfitz7@gmail.com>
2024-02-07 10:54:59 -0800

    fixing batches pt 3

--- eee/calcs/batch_running.py
+++ eee/calcs/batch_running.py
@@ -1,13 +1,29 @@
 #from eee.analysis.dms_epistasis import dms_epistasis
 
 import pandas as pd
-import subprocess
+import shutil
+import os
 
-def batch_run_dms_epi(on_off_df,prot_name:str):
+from eee.calcs import read_json
+
+def batch_run_dms_epi(json_file,
+                      midpoints:list,
+                      switch_range:list,
+                      prot_name:str,
+                      use_stored_seed=False,
+                      overwrite=False):
     """
     must be run in a file with an ensemble.csv, conditions.csv, simulation.json, ddg.csv.
     """
-    
+    on=[]
+    off=[]
+    for point in midpoints:
+        for number in switch_range:
+            on.append(point+number)
+            off.append(point-number)
+
+    on_off_df=pd.DataFrame(data=({'on':on,'off':off}))
+
     conditions_df=pd.read_csv('conditions.csv')
     for idx,row in on_off_df.iterrows():
         on=on_off_df['on'][idx]
@@ -18,25 +34,21 @@ def batch_run_dms_epi(on_off_df,prot_name:str):
 
         out_dir=prot_name+"_on_"+str(on)+'_off_'+str(off)
 
-        cmd=['eee-run-calculation','simulation.json',out_dir]
+        es, kwargs = read_json(json_file=json_file,
+                        use_stored_seed=use_stored_seed)
+        
+        if os.path.exists(out_dir):
+            if overwrite:
+                shutil.rmtree(out_dir)
+            else:
+                err = f"\noutput_directory '{out_dir}' already exists\n\n"
+                raise FileExistsError(err)
 
-        verbose=True
-
-        #using call instead of popen so that the whole thing is run before conditions.csv is rewritten
-        call= subprocess.call(cmd,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT,
-                             universal_newlines=True)
-
-        for line in call.stdout:
-            if verbose:
-                print(line,end="",flush=True)
-
-        # Check for success
-        return_code = call.wait()
-        if return_code != 0:
-            err = "Program failed.\n"
-            raise RuntimeError(err)
+    print()
+    print(es.get_calc_description(kwargs))
+    print(f"\nWriting result to '{out_dir}' directory\n",flush=True)
+            
+    es.run(output_directory=out_dir,**kwargs)
 
         #cannot run dms_epistasis within eee because it meses with importing
         #epi_df=dms_epistasis(prot_name+"_on_"+on_off_df['on'][idx]+'_off_'+on_off_df['off'][idx])

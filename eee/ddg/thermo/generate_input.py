a0a3a237112b5f9ff34a5d1cfc29b4e79498e34a
Brennan <brennanfitz7@gmail.com>
2024-09-16 16:04:31 -0700

    fixing thermo gen_input

--- eee/ddg/thermo/generate_input.py
+++ eee/ddg/thermo/generate_input.py
@@ -5,8 +5,9 @@ import glob
 
 def generate_input(list_of_pdbs:list, prot_name:str, all_models_necessary:bool):
     
+    new_prot_name=prot_name+'synced'
     sync_structures(list_of_pdbs, 
-                    out_dir=prot_name+'_synced', 
+                    out_dir=new_prot_name, 
                     all_models_necessary=all_models_necessary)
     
     synced_pdbs=glob.glob(prot_name+'/*.pdb')

c129d38b32195dcd5408d11b076162b8579a9f28
Brennan <brennanfitz7@gmail.com>
2025-02-20 17:07:46 -0800

    correct generate input

--- eee/ddg/thermo/generate_input.py
+++ eee/ddg/thermo/generate_input.py
@@ -5,12 +5,12 @@ import glob
 
 def generate_input(list_of_pdbs:list, prot_name:str, all_models_necessary:bool):
     
-    new_prot_name=prot_name+'_synced'
+    prot_name_synced=prot_name+'_synced'
     sync_structures(list_of_pdbs, 
                     out_dir=new_prot_name, 
                     all_models_necessary=all_models_necessary)
     
-    synced_pdbs=glob.glob(prot_name+'/*.pdb')
+    synced_pdbs=glob.glob(prot_name_synced+'/*.pdb')
 
     for pdb in synced_pdbs:
         chains_and_multipliers(pdb,save_dict=True)

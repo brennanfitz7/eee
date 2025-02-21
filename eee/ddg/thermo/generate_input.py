d4f3a6cd0831e8a53f1b60ea5d2b54043a8d0f81
Brennan <brennanfitz7@gmail.com>
2025-02-20 17:48:09 -0800

    further generate input fixing

--- eee/ddg/thermo/generate_input.py
+++ eee/ddg/thermo/generate_input.py
@@ -7,7 +7,7 @@ def generate_input(list_of_pdbs:list, prot_name:str, all_models_necessary:bool):
     
     prot_name_synced=prot_name+'_synced'
     sync_structures(list_of_pdbs, 
-                    out_dir=new_prot_name, 
+                    out_dir=prot_name_synced, 
                     all_models_necessary=all_models_necessary)
     
     synced_pdbs=glob.glob(prot_name_synced+'/*.pdb')
